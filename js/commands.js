ff3ff30c327f29cda8caf612e45f2b7723def260
Samuel Veronese <samuel@Air-di-Samuel.homenet.telecomitalia.it>
2022-12-18 21:07:34 +0100

    first commit

---
+++ js/commands.js
@@ -0,0 +1,38 @@
+var instagram = "https://www.instagram.com/samuel_veronese/";
+var github = "https://github.com/samuelveronese/";
+
+about = [
+    "<br>",
+    "Hey, I'm Samuel!",
+    "I'm a computer science student at the University of Verona.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'instagram      <a href="' + instagram + '" target="_blank">instagram/samuel_veronese' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/samuelveronese' + "</a>",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">about</span>          Who is Samuel?',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">github</span>         Open my GitHub',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    "   __                            _                                               ",
+    "  / _\\ __ _ _ __ ___  _   _  ___| | /\\   /\\___ _ __ ___  _ __   ___  ___  ___ ",
+    "  \\ \\ / _` | '_ ` _ \\| | | |/ _ \\ | \\ \\ / / _ \\ '__/ _ \\| '_ \\ / _ \\/ __|/ _ \\",
+    "  _\\ \\ (_| | | | | | | |_| |  __/ |  \\ V /  __/ | | (_) | | | |  __/\\__ \\  __/",
+    "  \\__/\\__,_|_| |_| |_|\\__,_|\\___|_|   \\_/ \\___|_|  \\___/|_| |_|\\___||___/\\___|",
+    "                                                                                     ",
+    '<span class="color2">Welcome...</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
